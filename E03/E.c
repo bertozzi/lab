1300d864c998bbccd55638636c0ce826e2c63d22
incuso <numismatica@gmail.com>
2024-10-14 14:45:53 +0200

    recuperate soluzioni E e F di E03

---
+++ E03/E.c
@@ -0,0 +1,22 @@
+// prendere ingresso un numero intero N, calcoli la somma di tutti i
+//  numeri pari da 0 a N e stamparla
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv){
+
+  int n;
+
+  printf("Inserisci un numero intero positivo: ");
+  scanf("%d", &n);
+
+  int acc = 0;
+  for(int i = 0; i <= n; i += 2) // in laboratorio molti incrementavano di 1 per poi pero' usare un if() per sommare i soli numeri pari
+    acc += i;
+
+  printf("La somma dei numeri pari tra 0 e %d vale %d\n", n, acc);
+
+  return 0;
+}
+
---
+++ E03/F.c
@@ -0,0 +1,30 @@
+//  F leggere da tastiera un numero, stampare quante cifre ha
+//   (suggerimento, dividere per 10 fino a che il risultato non è 0)
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv){
+
+  int n;
+
+  printf("Inserisci un numero intero positivo: ");
+  scanf("%d", &n);
+
+  int ncifre = 0;
+
+  printf("Il numero di cifre di %d e' ", n);
+
+  do
+  {
+    n = n/10;
+    ++ncifre;
+  }while(n);
+
+  printf("%d\n", ncifre);
+
+
+  return 0;
+}
+
